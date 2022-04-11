4f01c3223cc0b533bce70a20a5398f83e17ec5bf
Aayam <getaayam@gmail.com>
2022-04-11 09:42:03 +0545

    add dynamic props data

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
-import { Login } from "./pages/Login";
+import Login from "./pages/Login";
 import Post from "./pages/Post";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
+  const [user, setuser] = useState(true);
   return (
     <BrowserRouter>
       <div>
-        <NavBar />
+        <NavBar user={user} />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/post/:id" element={<Post  />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/post/:id"
+            element={user ? <Post /> : <Navigate to="/login" />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
