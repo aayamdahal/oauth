cdbefa70c9e2d07f9f92216744430429b6b7179f
Aayam <getaayam@gmail.com>
2022-04-14 21:01:02 +0545

    add logout route

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import "../styles/index.css";
 import img from "../styles/img.png";
 const NavBar = ({ user }) => {
+  const logout = () => {
+    window.open("http://localhost:5000/auth/logout", "_self");
+  };
   return (
     <div className="navbar">
       <Link className="logo" to="/">
@@ -13,7 +16,9 @@ const NavBar = ({ user }) => {
           <li className="listitem">
             <img src={img} className="avatar" alt="test" />
           </li>
-          <li className="listitem">Logout</li>
+          <li className="listitem" onClick={logout}>
+            Logout
+          </li>
         </ul>
       ) : (
         <Link to="login" className="link">
